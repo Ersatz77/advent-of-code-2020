f95fde52cb24b1246774214d6eb6b5adbf8d4b12
Ersatz <cm12mc@gmail.com>
2020-12-09 00:08:12 -0800

    Added solution for day 9

---
+++ advent-of-code-2020/src/day_10/day_10.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+
+void day_10_part_1()
+{
+
+}
+
+void day_10_part_2()
+{
+
+}
--- advent-of-code-2020/src/day_9/day_9.cpp
+++ advent-of-code-2020/src/day_9/day_9.cpp
@@ -1,12 +1,123 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <vector>
+#include <limits>
+
+std::vector<uint64_t> get_input(const std::string& path)
+{
+	std::ifstream file(path);
+	if (!file)
+	{
+		throw std::runtime_error("Unable to open file: " + path);
+	}
+
+	std::vector<uint64_t> input;
+	uint64_t number = 0;
+	while (file >> number)
+	{
+		input.push_back(number);
+	}
+
+	return input;
+}
+
+bool valid_next_number(const std::vector<uint64_t>& prev, uint64_t target)
+{
+	for (size_t i = 0; i < prev.size(); ++i)
+	{
+		for (size_t j = i + 1; j < prev.size(); ++j)
+		{
+			if (prev[i] != prev[j] && prev[i] + prev[j] == target)
+			{
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
+uint64_t find_invalid_number(const std::vector<uint64_t> code, size_t range_min, size_t range_max)
+{
+	uint64_t invalid_number = 0;
+	while (range_max + 1 < code.size() - 1)
+	{
+		std::vector<uint64_t> prev_range(code.begin() + range_min, code.begin() + range_max + 1);
+		if (!valid_next_number(prev_range, code[range_max + 1]))
+		{
+			invalid_number = code[range_max + 1];
+			break;
+		}
+
+		++range_min;
+		++range_max;
+	}
+
+	return invalid_number;
+}
 
 void day_9_part_1()
 {
+	std::vector<uint64_t> code = get_input("D:\\Repositories\\advent-of-code-2020\\advent-of-code-2020\\input\\day_9.txt");
 
+	uint64_t invalid_number = find_invalid_number(code, 0, 24);
+
+	std::cout << "Day 9 part 1 | Invalid number: " << invalid_number << '\n';
 }
 
 void day_9_part_2()
 {
+	std::vector<uint64_t> code = get_input("D:\\Repositories\\advent-of-code-2020\\advent-of-code-2020\\input\\day_9.txt");
 
+	// Get index of invalid number
+	uint64_t invalid_number = find_invalid_number(code, 0, 24);
+	size_t invalid_index = 0;
+	for (size_t i = 0; i < code.size(); ++i)
+	{
+		if (code[i] == invalid_number)
+		{
+			invalid_index = i;
+			break;
+		}
+	}
+
+	// Find contiguous set that adds up to the invalid number
+	std::vector<uint64_t> range;
+	bool found_range = false;
+	for (size_t i = 0; !found_range && i < invalid_index; ++i)
+	{
+		for (size_t j = i + 1; j < invalid_index; ++j)
+		{
+			range = std::vector<uint64_t>(code.begin() + i, code.begin() + j + 1);
+			uint64_t sum = 0;
+			for (const uint64_t value : range)
+			{
+				sum += value;
+			}
+
+			if (sum == invalid_number)
+			{
+				found_range = true;
+				break;
+			}
+		}
+	}
+
+	uint64_t min = std::numeric_limits<uint64_t>::max();
+	uint64_t max = std::numeric_limits<uint64_t>::min();
+	for (const uint64_t i : range)
+	{
+		if (i < min)
+		{
+			min = i;
+		}
+		
+		if (i > max)
+		{
+			max = i;
+		}
+	}
+
+	std::cout << "Day 9 part 2 | Encryption weakness: " << min + max << '\n';
 }
